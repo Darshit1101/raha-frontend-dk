c2c4c86ff4db0b88c49c925a50c1f913a2900c1b
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-06-13 11:55:19 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,23 @@ export default defineConfig({
     },
   },
 });
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       axiosApi: path.resolve(__dirname, "src/axiosApi"),
+//       components: path.resolve(__dirname, "src/components"),
+//       utils: path.resolve(__dirname, "src/utils"),
+//       hooks: path.resolve(__dirname, "src/hooks")
+//     }
+//   }
+// });
+
